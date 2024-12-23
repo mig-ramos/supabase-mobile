addb517cb038e6d52d3cd4a651bfa37a5cd465a3
Miguel Arcanjo <mig.ramos10@gmail.com>
2024-12-23 20:04:40 -0300

    feat: cria e limpa projeto

---
+++ constants/colors.ts
@@ -0,0 +1,10 @@
+const tintColorLight = '#2f95dc';
+const tintColorDark = '#fff';
+
+export default {
+zinc: '#0c0c1b',
+black: '#000',
+white: '#FFF',
+green: '#99CF1D',
+gray: '#DDDDDD',
+};
---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Login() {
+  return (
+    <View>
+      <Text>Página Login</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
