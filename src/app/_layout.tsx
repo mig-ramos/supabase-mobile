09759fb917ea55273ac0b4d1b366f330e376ab3a
Miguel Arcanjo <mig.ramos10@gmail.com>
2024-12-24 16:49:45 -0300

    cria as rotas em layout.tsx

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,6 +4,16 @@ export default function MainLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
+
+      <Stack.Screen
+        name="(auth)/signup/page"
+        options={{ headerShown: false }}
+      />
+
+      <Stack.Screen
+        name="(panel)/profile/page"
+        options={{ headerShown: false }}
+      />
     </Stack>
   );
 }
