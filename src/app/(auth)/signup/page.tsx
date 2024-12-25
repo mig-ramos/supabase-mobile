6fb7e283b3abd26c27587d4de4ced725da2bf68f
Miguel Arcanjo <mig.ramos10@gmail.com>
2024-12-25 11:38:06 -0300

    cria tela de cadastro

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,9 +1,43 @@
-import { StyleSheet, Text, View } from "react-native";
+import colors from "@/constants/colors";
+import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
+import { router } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 
-export default function SignUp() {
+export default function Signup() {
   return (
     <View style={styles.container}>
-      <Text>Página Cadastro</Text>
+      <View style={styles.header}>
+        <Pressable style={styles.backButton} onPress={() => router.back()}>
+          <Ionicons name="arrow-back" size={24} color={colors.white} />
+        </Pressable>
+        <Text style={styles.logoText}>
+          Dev<Text style={{ color: colors.green }}>App</Text>
+        </Text>
+        <Text style={styles.slogan}>Criar uma conta</Text>
+      </View>
+      <View style={styles.form}>
+        <View>
+          <Text style={styles.label}>Nome completo</Text>
+          <TextInput placeholder="Nome completo..." style={styles.input} />
+        </View>
+
+        <View>
+          <Text style={styles.label}>E-mail</Text>
+          <TextInput placeholder="Digite seu e-mail..." style={styles.input} />
+        </View>
+
+        <View>
+          <Text style={styles.label}>Senha</Text>
+          <TextInput
+            placeholder="Digite sua senha..."
+            style={styles.input}
+            secureTextEntry
+          />
+        </View>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>Cadastrar</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
@@ -11,7 +45,64 @@ export default function SignUp() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    paddingTop: 34,
+    backgroundColor: colors.zinc,
+  },
+  header: {
+    paddingLeft: 14,
+    paddingRight: 14,
+  },
+  logoText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: colors.white,
+    marginBottom: 8,
+  },
+  slogan: {
+    fontSize: 34,
+    color: colors.white,
+    marginBottom: 34,
+  },
+  form: {
+    flex: 1,
+    backgroundColor: colors.white,
+    borderTopLeftRadius: 16,
+    borderTopRightRadius: 16,
+    paddingTop: 24,
+    paddingLeft: 14,
+    paddingRight: 14,
+  },
+  label: {
+    color: colors.zinc,
+    marginBottom: 4,
+  },
+  input: {
+    borderWidth: 1,
+    borderBlockColor: colors.gray,
+    borderRadius: 8,
+    marginBottom: 16,
+    paddingHorizontal: 8,
+    paddingTop: 14,
+    paddingBottom: 14,
+  },
+  button: {
+    backgroundColor: colors.green,
+    paddingTop: 14,
+    paddingBottom: 14,
     alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    borderRadius: 8,
+  },
+  buttonText: {
+    color: colors.white,
+    fontWeight: "bold",
+  },
+  backButton: {
+    backgroundColor: "rgba(255, 255, 255, 0.55)",
+    alignSelf: "flex-start",
+    padding: 8,
+    borderRadius: 8,
+    marginBottom: 8,
   },
 });
